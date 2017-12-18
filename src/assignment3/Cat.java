5aee546e83908ea667dc5ab3f05fc42d0fd04cec
greenproton <greenproton@users.noreply.github.com>
2017-12-18 16:11:00 -0700

    See comments above

--- src/assignment3/Cat.java
+++ src/assignment3/Cat.java
@@ -14,15 +14,26 @@ public class Cat extends Animal {
     public Cat(int numberOfLegs, String speakParameters, String name) {
         super(numberOfLegs, speakParameters, name);
         name = "Cat";
-        numberOfLegs = 4;
-        speakParameters = "Meow";
+        numberOfLegs = 4;       // this is already set in the constructor - should not reset it.
+        speakParameters = "Meow";   // this is already set in the constructor - should not reset it.
         System.out.println(name+", "+numberOfLegs+", "+speakParameters);
+    }
+    
+    public Cat(String name) {       // this is the constructor we are looking for?
+        super(4, "Meow", name);
     }
 
     public void walkTo(int x, int y) {
         System.out.println("Walking");
         System.out.println("Lying down");
-        x = 0;
-        y = 1;
+        
+        // should be:
+        this.x = x;
+        this.y = y;
+        
+        //x = 0;                      // you do not want to set these here - they are set when they are called
+        //y = 1;
+        
+        
     }
 }
